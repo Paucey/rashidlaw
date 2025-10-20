da020608e4f729e4445112a857e731ec220ae391
ae-314 <aaron.elcheson@mail.mcgill.ca>
2025-10-20 19:53:27 -0400

    test push from aaron

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}, ah-ah-ah!
+          count is {count}, ah-ah-ah! BEEP BOOP BEEP BOOP
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
